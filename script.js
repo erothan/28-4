e16dd3890e63d34882bcf8cc1d2fff1541168a8d
Eross Thanatos <erossthanatos@gmail.com>
2022-09-28 23:55:18 +0330

    replace algorithm

--- script.js
+++ script.js
@@ -2,15 +2,8 @@ let num1 = Number(prompt("Enter first number"));
 let num2 = Number(prompt("Enter second number"));
 let num3 = Number(prompt("Enter third number"));
 
-//checking num1 is a middle number or not
-if (num2 > num1 && num1 > num3 || num3 > num1 && num1 > num2) {
-    alert(num1 + " is a middle number");
-}
-//checking num2 is a middle number or not
-if (num1 > num2 && num2 > num3 || num3 > num2 && num2 > num1) {
-    alert(num2 + " is a middle number");
-}
-//checking num3 is a middle number or not
-if (num1 > num3 && num3 > num2 || num2 > num3 && num3 > num1) {
-    alert(num3 + " is a middle number");
-}
+let min = Math.min(num1, num2, num3);
+let max = Math.max(num1,num2,num3) ;
+let mid = num1 + num2 + num3 - max -min;
+
+alert( mid + " is the middel of those numbers.");
